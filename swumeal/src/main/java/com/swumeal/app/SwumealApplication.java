26bf564743812a03a1f0af89ba493d0c5bc186b0
this-soyoung <this.soyoung@gmail.com>
2023-09-29 00:29:29 +0900

    basic setting

---
+++ swumeal/src/main/java/com/swumeal/app/SwumealApplication.java
@@ -0,0 +1,13 @@
+package com.swumeal.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwumealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwumealApplication.class, args);
+    }
+
+}
