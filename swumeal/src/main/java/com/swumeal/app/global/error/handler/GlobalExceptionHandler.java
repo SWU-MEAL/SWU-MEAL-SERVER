90ed2f721c64e0b3ff635361ff578eba770800e8
Yusun Choi <nsuy.ch@gmail.com>
2023-10-02 04:01:28 +0900

    feat: Add MenuApiException (#2)

--- swumeal/src/main/java/com/swumeal/app/global/error/handler/GlobalExceptionHandler.java
+++ swumeal/src/main/java/com/swumeal/app/global/error/handler/GlobalExceptionHandler.java
@@ -1,20 +1,35 @@
 package com.swumeal.app.global.error.handler;
 
 import com.swumeal.app.domain.menu.error.exception.DataUploadException;
+import com.swumeal.app.domain.menu.error.exception.MenuApiException;
 import com.swumeal.app.global.common.response.ResponseDto;
 import com.swumeal.app.global.error.GlobalErrorCode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.io.IOException;
 
 @RestControllerAdvice
 @Slf4j
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    // MenuApiException
+    @ExceptionHandler(MenuApiException.class)
+    public ResponseEntity<ResponseDto> exceptionHandler(MenuApiException e) {
+        HttpStatus httpStatus = e.getErrorCode().getHttpStatus();
+        String message = e.getMessage();
+
+        e.printStackTrace();
+
+        return ResponseEntity.status(httpStatus.value()).body(ResponseDto.of(httpStatus.value(), message));
+    }
+
     // DataUploadException
     @ExceptionHandler(DataUploadException.class)
     public ResponseEntity<ResponseDto> exceptionHandler(DataUploadException e) {
@@ -27,7 +42,7 @@ public class GlobalExceptionHandler {
     }
 
     // IOException
-    @ExceptionHandler(value = IOException.class)
+    @ExceptionHandler(IOException.class)
     public ResponseEntity<ResponseDto> exceptionHandler(IOException e) {
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
         String message = httpStatus.getReasonPhrase();
@@ -39,14 +54,15 @@ public class GlobalExceptionHandler {
     }
 
     // MethodNotAllowed
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<ResponseDto> exceptionHandler(HttpRequestMethodNotSupportedException e) {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers, HttpStatus status, WebRequest request) {
         HttpStatus httpStatus = GlobalErrorCode.METHOD_NOT_ALLOWED.getHttpStatus();
         String message = e.getMessage();
 
         e.printStackTrace();
 
         return ResponseEntity.status(httpStatus.value()).body(ResponseDto.of(httpStatus.value(), message));
+
     }
 
     // InternalServerError
