8bb68d889fc3e64712cec77c6cdfa1898fadf130
Yusun Choi <nsuy.ch@gmail.com>
2023-10-02 04:26:46 +0900

    fix: Combine domain/menu/dao/MenuDAO into dao/MenuDAO (#7)

--- swumeal/src/main/java/com/swumeal/app/dao/MenuDAO.java
+++ swumeal/src/main/java/com/swumeal/app/dao/MenuDAO.java
@@ -1,6 +1,8 @@
 package com.swumeal.app.dao;
 
 import com.swumeal.app.domain.dto.MenuDTO;
+import com.swumeal.app.domain.vo.MenuItemVO;
+import com.swumeal.app.domain.vo.MenuVO;
 import com.swumeal.app.mapper.MenuMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -12,13 +14,21 @@ import java.util.List;
 public class MenuDAO {
     private final MenuMapper menuMapper;
 
-//    오늘슈밥
-    public List<MenuDTO> find(){
+    public void save(MenuVO menuVO) {
+        menuMapper.insertMenu(menuVO);
+    }
+
+    public void save(MenuItemVO menuItemVO) {
+        menuMapper.insertMenuItem(menuItemVO);
+    }
+
+    //    오늘슈밥
+    public List<MenuDTO> find() {
         return menuMapper.select();
     }
 
-//    이번주 슈밥
-    public List<MenuDTO> findAll(){
+    //    이번주 슈밥
+    public List<MenuDTO> findAll() {
         return menuMapper.selectAll();
     }
 }
