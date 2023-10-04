56e8318b5fdab2f21a0a9a6f8b7e38ef14bca975
Yusun Choi <nsuy.ch@gmail.com>
2023-10-05 03:57:08 +0900

    test: Add findByTime Service Test (#9)

--- swumeal/src/test/java/com/swumeal/app/domain/menu/service/MenuReadServiceTest.java
+++ swumeal/src/test/java/com/swumeal/app/domain/menu/service/MenuReadServiceTest.java
@@ -1,19 +1,22 @@
 package com.swumeal.app.domain.menu.service;
 
 import com.swumeal.app.dao.MenuDAO;
+import com.swumeal.app.domain.menu.dto.DailyMenuDto;
 import com.swumeal.app.domain.menu.dto.DailyMenuListDto;
-import com.swumeal.app.domain.menu.dto.DateRequestDto;
 import com.swumeal.app.domain.menu.dto.MenuListByDateDto;
+import com.swumeal.app.domain.menu.dto.MenuListByTimeDto;
+import com.swumeal.app.domain.menu.dto.request.DateRequestDto;
 import com.swumeal.app.domain.vo.MenuItemVO;
 import com.swumeal.app.domain.vo.MenuVO;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,18 +32,16 @@ class MenuReadServiceTest {
     MenuDAO menuDAO;
 
     final ArrayList<MenuVO> menuList = new ArrayList<>();
-    final String DATE = "2025-10-15";
     final String TYPE_A = "샬롬", TYPE_B = "교직원";
     final String TIME_B = "조식", TIME_L = "중식", TIME_D = "석식";
     final String CORNER_A = "한식", CORNER_B = "일품", CORNER_C = "스낵";
     final String MENU = "메뉴";
 
-    @BeforeEach
-    void addMockData() {
+    void addMockData(String date) {
         // Add Menu
         for (int i = 0; i < 6; i++) {
             MenuVO menuVO = new MenuVO();
-            menuVO.setMealDate(DATE);
+            menuVO.setMealDate(date);
 
             menuList.add(menuVO);
         }
@@ -78,7 +79,9 @@ class MenuReadServiceTest {
     @DisplayName("findByDate service test")
     void findByDate() {
         // given
-        DateRequestDto dto = new DateRequestDto(DATE);
+        String date = "2025-10-15";
+        DateRequestDto dto = new DateRequestDto(date);
+        addMockData(date);
 
         // when
         MenuListByDateDto result = menuReadService.findByDate(dto);
@@ -87,9 +90,32 @@ class MenuReadServiceTest {
         // then
         log.info(result.toString());
 
-        assertThat(result.getDate()).as("[데이터 조회 실패] 날짜 불일치").isEqualTo(DATE);
+        assertThat(result.getDate()).as("[데이터 조회 실패] 날짜 불일치").isEqualTo(date);
         assertThat(list.size()).as("[데이터 조회 실패] 데이터 시간별 분류 총 건수 불일치").isEqualTo(3);
         assertThat(list.get(1).getMenuList().size()).as("[데이터 조회 실패] 데이터 식단별 분류 총 건수 불일치").isEqualTo(4);
         assertThat(list.get(2).getMenuList().get(0).getType()).as("[데이터 조회 실패] 데이터 식단별 분류 총 건수 불일치").isEqualTo(menuList.get(5).getMealType());
+    }
+
+    @Test
+    @DisplayName("findByTime service test")
+    void findByTime() {
+        // given
+        String date = LocalDate.now().format(DateTimeFormatter.ISO_DATE);
+        String time = "d";
+        String item = MENU + "2" + TIME_D + TYPE_A;
+        addMockData(date);
+
+        // when
+        MenuListByTimeDto result = menuReadService.findByTime(time);
+        ArrayList<DailyMenuDto> list = result.getResult();
+
+        // then
+        log.info(result.toString());
+
+        assertThat(result.getDate()).as("[데이터 조회 실패] 날짜 불일치").isEqualTo(date);
+        assertThat(result.getTime()).as("[데이터 조회 실패] 시간 불일치").isEqualTo(TIME_D);
+        assertThat(list.size()).as("[데이터 조회 실패] 데이터 총 건수 불일치").isEqualTo(1);
+        assertThat(list.get(0).getType()).as("[데이터 조회 실패] 식단 타입 불일치").isEqualTo(TYPE_A);
+        assertThat(list.get(0).getItems().get(2)).as("[데이터 조회 실패] 데이터 식단별 분류 총 건수 불일치").isEqualTo(item);
     }
 }
