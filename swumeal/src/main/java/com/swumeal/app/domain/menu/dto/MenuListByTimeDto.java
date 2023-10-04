94750e1e4b6ff5c8c39821be59dd657e37a177ff
Yusun Choi <nsuy.ch@gmail.com>
2023-10-05 03:21:38 +0900

    feat: Add findByTime Service (#9)

--- swumeal/src/main/java/com/swumeal/app/domain/menu/dto/MenuListByTimeDto.java
+++ swumeal/src/main/java/com/swumeal/app/domain/menu/dto/MenuListByTimeDto.java
@@ -1,14 +1,29 @@
 package com.swumeal.app.domain.menu.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 
 @Getter
-@AllArgsConstructor
+@Setter
+@NoArgsConstructor
 public class MenuListByTimeDto {
     private String date;
     private String time;
     private ArrayList<DailyMenuDto> result;
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("{\n")
+                .append(time)
+                .append("\n");
+
+        for (DailyMenuDto menu : result)
+            builder.append(menu.toString());
+
+        return builder.append("\n}").toString();
+    }
 }
--- swumeal/src/main/java/com/swumeal/app/domain/menu/service/MenuReadService.java
+++ swumeal/src/main/java/com/swumeal/app/domain/menu/service/MenuReadService.java
@@ -1,8 +1,12 @@
 package com.swumeal.app.domain.menu.service;
 
 import com.swumeal.app.dao.MenuDAO;
-import com.swumeal.app.domain.menu.dto.DateRequestDto;
 import com.swumeal.app.domain.menu.dto.MenuListByDateDto;
+import com.swumeal.app.domain.menu.dto.MenuListByTimeDto;
+import com.swumeal.app.domain.menu.dto.request.DateRequestDto;
+import com.swumeal.app.domain.menu.error.exception.MenuApiException;
+import com.swumeal.app.domain.model.TimeEnum;
+import com.swumeal.app.global.error.GlobalErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,5 +19,22 @@ public class MenuReadService {
 
     public MenuListByDateDto findByDate(DateRequestDto date) {
         return menuDAO.findByDate(date.getDate());
+    }
+
+    public MenuListByTimeDto findByTime(String timeStr) {
+        TimeEnum time;
+
+        // time 값 검사
+        switch (timeStr) {
+            case "b", "breakfast" -> time = TimeEnum.BREAKFAST;
+            case "l", "lunch" -> time = TimeEnum.LUNCH;
+            case "d", "dinner" -> time = TimeEnum.DINNER;
+            default -> time = null;
+        }
+
+        if (time == null)
+            throw new MenuApiException(GlobalErrorCode.INVALID_TIME_FORMAT);
+
+        return menuDAO.findByTime(time.getKey());
     }
 }
