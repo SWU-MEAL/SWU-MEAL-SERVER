1f32a1a2b30503e931fd8b94c1614c1298aa3a7c
Yusun Choi <nsuy.ch@gmail.com>
2023-10-05 02:08:39 +0900

    feat: Add DAO for data by date (#9)

--- swumeal/src/main/java/com/swumeal/app/mapper/MenuMapper.java
+++ swumeal/src/main/java/com/swumeal/app/mapper/MenuMapper.java
@@ -1,9 +1,9 @@
 package com.swumeal.app.mapper;
 
+import com.swumeal.app.domain.dto.MenuDTO;
+import com.swumeal.app.domain.menu.dto.MenuListByDateDto;
 import com.swumeal.app.domain.vo.MenuItemVO;
 import com.swumeal.app.domain.vo.MenuVO;
-import com.swumeal.app.domain.dto.MenuDTO;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -16,9 +16,12 @@ public interface MenuMapper {
     // menu item 저장
     void insertMenuItem(MenuItemVO menuItemVO);
 
-//  오늘 날짜 슈밥 조회
-    public List<MenuDTO> select();
-// 이번주 슈밥 조회
-    public List<MenuDTO> selectAll();
-  
+    //  오늘 날짜 슈밥 조회
+    List<MenuDTO> select();
+
+    // 이번주 슈밥 조회
+    List<MenuDTO> selectAll();
+
+    // 날짜 기준 슈밥 조회
+    MenuListByDateDto selectByDate(String date);
 }
