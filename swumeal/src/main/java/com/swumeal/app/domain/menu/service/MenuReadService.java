d3c135faf8db9cd13e6556e3c550e3e158773969
Yusun Choi <nsuy.ch@gmail.com>
2023-10-05 02:28:33 +0900

    fix: Remove `@RequestParam`, `@Valid` on findByDate Service (#9)

--- swumeal/src/main/java/com/swumeal/app/domain/menu/service/MenuReadService.java
+++ swumeal/src/main/java/com/swumeal/app/domain/menu/service/MenuReadService.java
@@ -6,9 +6,6 @@ import com.swumeal.app.domain.menu.dto.MenuListByDateDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.validation.Valid;
 
 @Service
 @Slf4j
@@ -16,7 +13,7 @@ import javax.validation.Valid;
 public class MenuReadService {
     private final MenuDAO menuDAO;
 
-    public MenuListByDateDto findByDate(@RequestParam @Valid DateRequestDto date) {
+    public MenuListByDateDto findByDate(DateRequestDto date) {
         return menuDAO.findByDate(date.getDate());
     }
 }
