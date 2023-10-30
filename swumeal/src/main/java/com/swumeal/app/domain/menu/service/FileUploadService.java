bbef7c82d26fb0d4b5b154fc980cd34cff59fc29
Yusun Choi <nsuy.ch@gmail.com>
2023-10-30 22:09:56 +0900

    feat: Add `@transactional` on FileUploadService (#27)

--- swumeal/src/main/java/com/swumeal/app/domain/menu/service/FileUploadService.java
+++ swumeal/src/main/java/com/swumeal/app/domain/menu/service/FileUploadService.java
@@ -11,6 +11,7 @@ import com.swumeal.app.global.util.UploadedFileProcessor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -23,6 +24,7 @@ public class FileUploadService {
     private final UploadedFileProcessor processor;
     private final MenuDAO menuDAO;
 
+    @Transactional
     public LinkedList<MenuDataVo> uploadData(MultipartFile file, String type) throws IOException {
         MealTypeEnum typeEnum;
         switch (type) {
