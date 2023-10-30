540bf647b56442589c938cc0a301005867cadf99
Yusun Choi <nsuy.ch@gmail.com>
2023-10-30 22:05:20 +0900

    feat: Remove created_at, updated_at on PolicyVO (#27)

--- swumeal/src/main/java/com/swumeal/app/domain/mypage/domain/PolicyVO.java
+++ swumeal/src/main/java/com/swumeal/app/domain/mypage/domain/PolicyVO.java
@@ -5,8 +5,6 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-
 @Slf4j
 @Component
 @Data
@@ -16,6 +14,4 @@ public class PolicyVO {
     private Integer num;
     private String title;
     private String content;
-    private LocalDateTime created_at;
-    private LocalDateTime updated_at;
 }
